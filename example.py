c8889cb70a6f1f864823373eda3e8270285816b9
arlencai <arlencai@tencent.com>
2019-10-31 12:53:28 +0800

    update cut

--- example.py
+++ example.py
@@ -3,13 +3,16 @@
 '''
 @Author: ArlenCai
 @Date: 2019-10-30 13:06:27
-@LastEditTime: 2019-10-30 13:15:30
+@LastEditTime: 2019-10-30 20:32:44
 '''
 from cppjiebadat_py import jieba 
 
 def main():
 
-    jieba_instance = jieba("cppjiebadat/dict/user.dict.utf8")
+    HMM_PATH = "cppjiebadat/dict/hmm_model.utf8"
+    IDF_PATH = "cppjiebadat/dict/idf.utf8"
+    STOP_WORD_PATH = "cppjiebadat/dict/stop_words.utf8"
+    jieba_instance = jieba("cppjiebadat/dict/jieba.dict.utf8", HMM_PATH, "cppjiebadat/dict/user.dict.utf8", IDF_PATH, STOP_WORD_PATH)
     seg_list = jieba_instance.cut("我来到北京清华大学")
     print("Full Mode: " + "/ ".join(seg_list))  # 全模式
 
@@ -21,6 +24,9 @@ def main():
         "小明硕士毕业于中国科学院计算所，后在日本京都大学深造")  # 搜索引擎模式
     print(", ".join(seg_list))
     
+    seg_list = jieba_instance.tag("我来到北京清华大学")
+    print(seg_list)
+
 
 if __name__ == '__main__':
     main()
--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,52 @@
 /*
  * @Author: ArlenCai
  * @Date: 2019-10-30 13:06:27
- * @LastEditTime: 2019-10-30 13:17:25
+ * @LastEditTime: 2019-10-30 20:28:41
  */
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include "cppjieba/Jieba.hpp"
 #include <iostream>
 
+using namespace std;
 namespace py = pybind11;
 
-const std::string DICT_PATH = "cppjiebadat/dict/jieba.dict.utf8";
-const std::string HMM_PATH = "cppjiebadat/dict/hmm_model.utf8";
-const std::string IDF_PATH = "cppjiebadat/dict/idf.utf8";
-const std::string STOP_WORD_PATH = "cppjiebadat/dict/stop_words.utf8";
+const string DICT_PATH = "cppjiebadat/dict/jieba.dict.utf8";
+const string HMM_PATH = "cppjiebadat/dict/hmm_model.utf8";
+const string IDF_PATH = "cppjiebadat/dict/idf.utf8";
+const string STOP_WORD_PATH = "cppjiebadat/dict/stop_words.utf8";
 
 struct JiebaCpp
 {
     cppjieba::Jieba jieba;
 
   public:
-    JiebaCpp(const std::string &USER_DICT_PATH) : jieba(DICT_PATH, HMM_PATH, USER_DICT_PATH, IDF_PATH, STOP_WORD_PATH){};
+    JiebaCpp(const string &USER_DICT_PATH) : jieba(DICT_PATH, HMM_PATH, USER_DICT_PATH, IDF_PATH, STOP_WORD_PATH){};
+    JiebaCpp(const string &DICT_PATH, const string &USER_DICT_PATH) : jieba(DICT_PATH, HMM_PATH, USER_DICT_PATH, IDF_PATH, STOP_WORD_PATH){};
+    JiebaCpp(const string &DICT_PATH, const string &HMM_PATH, const string &USER_DICT_PATH, const string &IDF_PATH, const string &STOP_WORD_PATH) : jieba(DICT_PATH, HMM_PATH, USER_DICT_PATH, IDF_PATH, STOP_WORD_PATH){};
 
-    std::vector<std::string> cut(std::string &text)
+    vector<string> cut(string &text)
     {
-        std::vector<std::string> words;
+        vector<string> words;
         jieba.Cut(text, words, true);
         return words;
     };
 
-    std::vector<std::string> cutForSearch(std::string &text)
+    vector<string> cutForSearch(string &text)
     {
-        std::vector<std::string> words;
+        vector<string> words;
         jieba.CutForSearch(text, words, true);
         return words;
     };
+
+    vector<pair<string, string>> tag(const string &sentence)
+    {
+        vector<pair<string, string> > words;
+        jieba.Tag(sentence, words);
+        return words;
+    };
+
+
     /*
     void InsertUserWord(std::string &word)
     {
@@ -48,8 +60,10 @@ PYBIND11_MODULE(cppjiebadat_py, m)
     m.doc() = "python extension for cppjiebadat"; // optional module docstring
 
     py::class_<JiebaCpp>(m, "jieba")
-        .def(py::init<const std::string &>())
+        .def(py::init<const string &>())
+        .def(py::init<const string &, const string &>())
+        .def(py::init<const string &, const string &, const string &, const string &, const string &>())
         .def("cut", &JiebaCpp::cut)
-        .def("cut_for_search", &JiebaCpp::cutForSearch);
-        //.def("add_word", &JiebaCpp::InsertUserWord);
+        .def("cut_for_search", &JiebaCpp::cutForSearch)
+        .def("tag", &JiebaCpp::tag);
 }
